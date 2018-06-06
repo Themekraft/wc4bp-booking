d3d3fc7c56819152ff52b5568f0d6fc2f06cbaf1
Victor Marin <marin250189@gmail.com>
2018-06-05 22:37:36 -0400

    initial commit

---
+++ classes/wc4bp_booking_integration.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @package        WordPress
+ * @subpackage     BuddyPress, Woocommerce, WC4BP
+ * @author         ThemKraft Dev Team
+ * @copyright      2017, Themekraft
+ * @link           http://themekraft.com/store/woocommerce-buddypress-integration-wordpress-plugin/
+ * @license        http://www.opensource.org/licenses/gpl-2.0.php GPL License
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class wc4bp_booking_integration {
+
+    public function __construct() {
+
+
+    }
+
+    /**
+     * Expose to the core where will be the function to pass to BP to load the template
+     *
+     * @param $id
+     * @param $title
+     * @param $screen_function
+     *
+     * @return array
+     */
+
+
+    /**
+     * This function tell to buddyPress where load the template of the plugin
+     */
+
+
+    /**
+     * TODO Complete by victor
+     *
+     * @param $view_subscription_url
+     * @param $id
+     *
+     * @return string
+     */
+
+
+    /**
+     * TODO Complete by victor
+     *
+     * @param $dir
+     *
+     * @return string
+     */
+
+
+    /**
+     * TODO Complete by victor
+     *
+     * @param $path
+     * @param $template_directory
+     *
+     * @return string
+     */
+
+
+    /**
+     * TODO Complete by victor
+     *
+     * @param $menu_items
+     *
+     * @return The
+     */
+
+
+    /**
+     * TODO Complete by victor
+     *
+     * @param $attr
+     * @param string $content
+     */
+
+
+    /**
+     * TODO Complete by victor
+     *
+     * @param $attr
+     * @param $content
+     */
+
+}
---
+++ classes/wc4bp_booking_manager.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @package        WordPress
+ * @subpackage     BuddyPress, Woocommerce, WC4BP
+ * @author         ThemKraft Dev Team
+ * @copyright      2017, Themekraft
+ * @link           http://themekraft.com/store/woocommerce-buddypress-integration-wordpress-plugin/
+ * @license        http://www.opensource.org/licenses/gpl-2.0.php GPL License
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class wc4bp_booking_manager {
+
+    private static $plugin_slug = 'wc4bp_booking';
+    protected static $version = '1.1.0';
+    private $end_points;
+
+    public function __construct() {
+        require_once WC4BP_SUBSCRIPTION_CLASSES_PATH . 'wc4bp_booking_log.php';
+        new wc4bp_booking_log();
+        try {
+            //loading_dependency
+            require_once WC4BP_SUBSCRIPTION_CLASSES_PATH . 'wc4bp_booking_integration.php';
+            new wc4bp_booking_integration();
+
+        } catch ( Exception $ex ) {
+            wc4bp_booking_log::log( array(
+                'action'         => get_class( $this ),
+                'object_type'    => self::getSlug(),
+                'object_subtype' => 'loading_dependency',
+                'object_name'    => $ex->getMessage(),
+            ) );
+        }
+    }
+
+    /**
+     * Get plugins version
+     *
+     * @return mixed
+     */
+    static function getVersion() {
+        return self::$version;
+    }
+
+    /**
+     * Get plugins slug
+     *
+     * @return string
+     */
+    static function getSlug() {
+        return self::$plugin_slug;
+    }
+
+    /**
+     * Retrieve the translation for the plugins. Wrapper for @see __()
+     *
+     * @param $str
+     *
+     * @return string
+     */
+    public static function translation( $str ) {
+        return __( $str, 'wc4bp_booking' );
+    }
+
+
+    /**
+     * Display the translation for the plugins. Wrapper for @see _e()
+     *
+     * @param $str
+     */
+    public static function echo_translation( $str ) {
+        _e( $str, 'wc4bp_booking' );
+    }
+
+    /**
+     * Display the translation for the plugins.
+     *
+     * @param $str
+     */
+    public static function echo_esc_attr_translation( $str ) {
+        echo esc_attr( self::translation( $str ) );
+    }
+}
