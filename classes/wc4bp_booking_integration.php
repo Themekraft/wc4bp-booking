948a6e2633076339359bd47beefc064063368cce
Victor Marin <marin250189@gmail.com>
2018-06-12 22:21:19 -0400

    add booking endpoint

--- classes/wc4bp_booking_integration.php
+++ classes/wc4bp_booking_integration.php
@@ -15,7 +15,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 class wc4bp_booking_integration {
 
     public function __construct() {
+        add_filter( 'wc4bp_add_endpoint', array( $this, 'wc4bp_booking_menu_items' ) );
 
+    }
 
+    public function wc4bp_booking_menu_items( $menu_items ) {
+        // Add our menu item after the Orders tab if it exists, otherwise just add it to the end
+        if ( array_key_exists( 'orders', $menu_items ) ) {
+            $menu_items = wcs_array_insert_after( 'orders', $menu_items, 'bookings', __( 'Bookings', 'woocommerce-bookings' ) );
+        } else {
+            $menu_items['bookings'] = __( 'Bookings', 'woocommerce-bookings' );
+        }
+
+        return $menu_items;
     }
 }
